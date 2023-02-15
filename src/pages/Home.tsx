388eacd7647e7d74f9d6feef5b1d8937960fc9d4
Pedro de Paula <pmdpaula@gmail.com>
2023-02-14 21:57:42 -0300

    fim desafio complementar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, StyleSheet, View } from "react-native";
 
 import { Header } from "../components/Header";
 import { Task, TasksList } from "../components/TasksList";
@@ -8,7 +8,12 @@ import { TodoInput } from "../components/TodoInput";
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
-  function handleAddTask(newTaskTitle: string) {
+  const handleAddTask = (newTaskTitle: string) => {
+    if (tasks.find((task) => task.title === newTaskTitle)) {
+      Alert.alert("Task já cadastrada", "Você não pode cadastrar uma task com o mesmo nome");
+      return;
+    }
+
     const data = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -16,19 +21,36 @@ export function Home() {
     };
 
     setTasks((oldTask) => [...oldTask, data]);
-  }
+  };
 
-  function handleToggleTaskDone(id: number) {
-    const updatedTasks = tasks.map((task) =>
+  const handleToggleTaskDone = (id: number) => {
+    const updatedTask = tasks.map((task) =>
       task.id === id ? { ...task, done: !task.done } : task
     );
 
-    setTasks(updatedTasks);
-  }
+    setTasks(updatedTask);
+  };
 
-  function handleRemoveTask(id: number) {
-    setTasks((oldTask) => oldTask.filter((task) => task.id !== id));
-  }
+  const handleRemoveTask = (id: number) => {
+    Alert.alert("Remover item", "Tem certeza que você deseja remover esse item?", [
+      {
+        text: "Não",
+        style: "cancel",
+      },
+      {
+        text: "Sim",
+        onPress: () => setTasks((oldTask) => oldTask.filter((task) => task.id !== id)),
+      },
+    ]);
+  };
+
+  const handleEditTask = (taskId: number, taskNewTitle: string) => {
+    const updatedTask = tasks.map((task) =>
+      task.id === taskId ? { ...task, title: taskNewTitle } : task
+    );
+
+    setTasks(updatedTask);
+  };
 
   return (
     <View style={styles.container}>
@@ -40,6 +62,7 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   );
